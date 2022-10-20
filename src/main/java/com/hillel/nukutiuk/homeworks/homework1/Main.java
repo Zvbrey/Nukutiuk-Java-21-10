ba4752fccb9dc0b8f3dade60d696adef05227f92
Nukutiuk Oleksander <zvbrey@gmail.com>
2022-10-20 20:59:25 +0300

    Homework

---
+++ src/main/java/com/hillel/nukutiuk/homeworks/homework1/Main.java
@@ -0,0 +1,8 @@
+package com.hillel.nukutiuk.homeworks.homework1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+
+    }
+}
---
+++ src/main/java/com/hillel/nukutiuk/homeworks/homework2/Main.java
@@ -0,0 +1,8 @@
+package com.hillel.nukutiuk.homeworks.homework2;
+
+public class Main {
+    public static void main(String[] args) {
+        double latitude =  50.48475720173383, longitude = 30.473543237326318 ;
+        System.out.println(latitude + " " + longitude );
+    }
+}
