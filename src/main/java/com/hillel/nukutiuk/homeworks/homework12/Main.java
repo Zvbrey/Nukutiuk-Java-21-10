fdcc15fc6ef5365f28dee795ce6c4bea5c7886e1
Nukutiuk Oleksander <zvbrey@gmail.com>
2022-12-04 20:29:09 +0200

    Homework

--- src/main/java/com/hillel/nukutiuk/homeworks/homework12/Main.java
+++ src/main/java/com/hillel/nukutiuk/homeworks/homework12/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println(Person.personInfo("Joe", "Hanjy", "Roza", "0939862359"));
         System.out.println(Person.personInfo("Zig", "Eger", "Rebelio", "09397360258"));
         System.out.println();
-        System.out.println(firstPerson +"\n"+ secondPerson +"\n"+ thirdPerson);
+        System.out.println(firstPerson + "\n" + secondPerson + "\n" + thirdPerson);
         //Не зрозумів що сама мається на увазі під  "Повинен вийти висновок такого типу" тому зробив два варіанта
     }
 
